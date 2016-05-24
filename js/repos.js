5ae64d4ca8e6f0911051c8e0f46aaf90fbd8d375
Jeffrey Gebhardt <jeff@gebbey.com>
2016-05-24 15:36:07 -0700

    stuff

---
+++ js/repos.js
@@ -0,0 +1,23 @@
+var repos = {};
+
+repos.all = [];
+
+repos.requestRepos = function(callback) {
+  $.ajax({
+    url: 'https://api.github.com/users/jeffgebhardt/repos' +
+        '?per_page=10' +
+        '&sort=updated',
+    type: 'GET',
+    headers: {'Authorization': 'token ' + myToken},
+    success: function(data, message, xhr) {
+      console.log(data);
+      repos.all = data;
+    }
+  });
+};
+
+repos.with = function(attr) {
+  return repos.all.filter(function(repo) {
+    return repo[attr];
+  });
+};
